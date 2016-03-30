24fe0d3e43e15882d2cbb96276b224fd6ed8cc43
dv-abhishek <dv.abhishek@gmail.com>
2016-03-30 23:30:26 +0530

    Removed admin model, added dynamic content to donor/viewEvents

--- donate/admin.py
+++ donate/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from models import *
 # Register your models here.
 admin.site.register(Donor)
-admin.site.register(Admin)
 admin.site.register(Volunteer)
 admin.site.register(NGO)
 
